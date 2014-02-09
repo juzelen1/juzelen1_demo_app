cf61845565275871d2f768e4b7d4c6f8a6910a8f
Charles Juzelenos <juzelen1@umbc.edu>
2014-02-09 17:50:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Juzelen1DemoApp::Application.config.secret_key_base = '1e8f3032ced972d80de4b90856be15d571294a982b623ce61762e25e2fd631efac65c66b72d4bb88158d9660e06d05d7d02a1f2ca2e60ef7186cd2f179531d30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Juzelen1DemoApp::Application.config.session_store :cookie_store, key: '_juzelen1_demo_app_session'
